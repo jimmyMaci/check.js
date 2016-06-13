e2c906518750e1c4882c28f9cf78ccfdd6d3a163
jimmyMaci <tyler.durden69@gmx.de>
2016-06-13 15:57:57 +0200

    Update check.js

--- check.js
+++ check.js
@@ -3,6 +3,7 @@ var Check = function() {
 	var self = this;
 	self.isUndefined = _isUndefined;
 	self.isNull = _isNull;
+	self.isObject = _isObject;
 	
 	function _isUndefined(value) {
 	  return (typeof value === "undefined");
@@ -11,4 +12,9 @@ var Check = function() {
 	function _isNull(value) {
 		return value == null;
 	}
+	
+	function _isObject(value) {
+		return (typeof value === "object");
+	}
+	
 }
